0953774f5e49f7a79fe88acaaf1bb372759c9fb6
GA-MO <ga-mo@GA-MOs-MacBook-Pro.local>
2018-03-22 14:50:14 +0700

    Add CI

--- dev/modules/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import todo from './todo'
-import user from './user'
-
-export default combineReducers({
-  todo: todo.state,
-  user: user.state,
-})
--- dev/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux'
-import reducers from '../modules'
-
-export default () => {
-  return createStore(reducers)
-}
